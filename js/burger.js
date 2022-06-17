5a55c5f615f99fe7df448fb26214c15de6c2e851
hrytsenko92 <m.hrytsenko1802@gmail.com>
2022-06-17 21:48:25 +0300

    fix benefits block.

--- js/burger.js
+++ js/burger.js
@@ -18,7 +18,6 @@ function myFunctionMain() {
   $(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
       $('.nav').addClass('affix');
-      console.log("OK");
     } else {
       $('.nav').removeClass('affix');
     }
