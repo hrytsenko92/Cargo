e7236974247e36054c6d52eb9895e24f513ce423
hrytsenko92 <m.hrytsenko1802@gmail.com>
2022-06-17 15:33:58 +0300

    add photo uploader style. fix bugs

--- js/cost-calculation.js
+++ js/cost-calculation.js
@@ -4,28 +4,58 @@ function resetImg (){
         document.getElementById("upload-input-wrap").style.display = "block";
 }
 
-function changeHandler(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+function tab01 (){
+    document.getElementById("two").disabled = false;
+    document.getElementById("two").checked = true;
+}
 
-    let files = evt.target.files;
+function tab02 (){
+    document.getElementById("one").disabled = false;
+    document.getElementById("one").checked = true;
+}
 
-    let file = files[0];
-
-    let fileReader = new FileReader();
-
-    fileReader.onload = function(progressEvent) {
-        let url = fileReader.result;
-
-        let myImg = document.getElementById("myimage");
-        myImg.src = url;
-        document.getElementById("upload-input-wrap").style.display = "none";
-    }
-
-    fileReader.readAsDataURL(file); 
+function tab03 (){
+    document.getElementById("one").disabled = true;
+    document.getElementById("two").disabled = true;
+    document.getElementById("three").disabled = true;
+    document.getElementById("three").checked = true;
+    document.getElementById("tabs").style.display = "none"; 
+    document.getElementById("three-panel").style.backgroundColor= "#282b38";
+    // document.getElementById("panels").style.padding = "0px 0px 0px 0px";
+    document.getElementById("panels").style.backgroundColor= "#282b38";
 }
 
 
 
 
- 
+// func changeHandler show uploadet photo. work, just turn off;
+
+// function changeHandler(evt) {   
+//     evt.stopPropagation();
+//     evt.preventDefault();
+
+//     let files = evt.target.files;
+
+//     let file = files[0];
+
+//     let fileReader = new FileReader();
+
+//     fileReader.onload = function(progressEvent) {
+//         let url = fileReader.result;
+
+//         let myImg = document.getElementById("myimage");
+//         myImg.src = url;
+//         document.getElementById("upload-input-wrap").style.display = "none";
+//     }
+
+//     fileReader.readAsDataURL(file); 
+// }  
+
+
+
+function changeHandler(evt) {
+        document.getElementById("myimage").style.display = "block";
+        document.getElementById("myimage").style.width = "32px"
+        document.getElementById("upload-input-wrap").style.display = "none";
+        document.getElementsByClassName("upload-photo-form__input")[0].style.border = "none";
+    }  
